70eddfc425a6ad5940f73037d73f6f813c4f5930
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 09:40:06 -0700

    Pass firt test for Age constructor

---
+++ src/js/galatic-age.js
@@ -0,0 +1,24 @@
+export default class Age {
+  constructor(earth) {
+    this.earth = earth;
+    this.mercury = earth * 0.24;
+    this.venus = earth * 0.62;
+    this.mars = earth * 1.88;
+    this.jupiter = earth * 11.86;g
+  }
+
+  // checkType() {
+  //   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+  //     return "not a triangle";
+  //   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+  //     return "scalene triangle";
+  //   }  else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+  //     return "equilateral triangle";
+  //   } else {
+  //     return "isosceles triangle";
+  //   }
+  // }  
+  
+  
+
+}
