2d4843e54e50bc81267c89d672f32fd0fc00b9d8
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 12:47:24 -0700

    Add third test

--- __tests__/galatic-age.test.js
+++ __tests__/galatic-age.test.js
@@ -12,7 +12,10 @@ describe('Age', () => {
     expect(galatic.getPastYears()).toEqual({earthYears: 13, mercuryYears: 54.16, venusYears: 20.96, marsYears: 6.91, jupiterYears: 1.09});
   });
 
-
+  test('should correctly calculate how many years to pass until a future birthday', () => {
+    const galatic = new Age(56, 61, 43);
+    expect(galatic.getFutureYears()).toEqual({earthYears: 5, mercuryYears: 20.83, venusYears: 8.06, marsYears: 2.65, jupiterYears: 0.42});
+  });
 
 
 });
