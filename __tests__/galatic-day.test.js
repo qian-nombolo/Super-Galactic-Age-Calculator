80a63ae14dac5d3eed1c3e227079c974a3718956
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 15:31:52 -0700

    Update forth test to create new class

--- __tests__/galatic-day.test.js
+++ __tests__/galatic-day.test.js
@@ -2,9 +2,9 @@ import Day from './../src/js/galatic-day.js';
 
 describe('Day', () => {
 
-  test('should correctly calculate how many days to pass until a future birthday', () => {
-    expect(galatic.getFutureDays().mercuryDays).toEqual(87.6);
+  test('should correctly create a Day object with year, month and date', () => {
+    const birthday = new Day(1993, 10, 01);
+    expect(birthday.month).toEqual(10);
   });
-
 
 });
