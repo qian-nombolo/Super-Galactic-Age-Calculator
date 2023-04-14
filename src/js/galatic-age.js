66d9bd675bc08ace6a21ed00165fd7d9c8f20a44
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 11:54:17 -0700

    Pass second test

--- src/js/galatic-age.js
+++ src/js/galatic-age.js
@@ -9,17 +9,21 @@ export default class Age {
     this.jupiter = earth * 11.86;
   }
 
-  // checkType() {
-  //   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
-  //     return "not a triangle";
-  //   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
-  //     return "scalene triangle";
-  //   }  else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
-  //     return "equilateral triangle";
-  //   } else {
-  //     return "isosceles triangle";
-  //   }
-  // }  
+  getPastYears() {
+    let earthYears = this.earth - this.pastB;
+    let mercuryYears = Math.floor((earthYears / 0.24)*100) / 100;
+    let venusYears = Math.floor((earthYears / 0.62)* 100) / 100;
+    let marsYears = Math.floor((earthYears / 1.88)* 100) / 100;
+    let jupiterYears = Math.floor((earthYears / 11.86)* 100) / 100;
+
+    return {
+      earthYears: earthYears,
+      mercuryYears: mercuryYears,
+      venusYears: venusYears,
+      marsYears: marsYears,
+      jupiterYears: jupiterYears
+    };
+  }  
   
   
 
