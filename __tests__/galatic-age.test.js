054d63ae98c2b52c90625925bfbccf2c2e1c2aa5
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 14:26:21 -0700

    Refactor the tests

--- __tests__/galatic-age.test.js
+++ __tests__/galatic-age.test.js
@@ -1,21 +1,23 @@
 import Age from './../src/js/galatic-age.js';
 
+let galatic;
+
+  beforeEach(() => {
+  galatic = new Age(56, 61, 43);
+  });
+
 describe('Age', () => {
 
   test('should correctly create a Age object with three ages', () => {
-    const galatic = new Age(10, 15, 5);
-    expect(galatic.futureB).toEqual(15);
+    expect(galatic.futureB).toEqual(61);
   });
 
   test('should correctly calculate how many years have passed on each planet since a past birthday', () => {
-    const galatic = new Age(56, 61, 43);
     expect(galatic.getPastYears()).toEqual({earthYears: 13, mercuryYears: 54.16, venusYears: 20.96, marsYears: 6.91, jupiterYears: 1.09});
   });
 
   test('should correctly calculate how many years to pass until a future birthday', () => {
-    const galatic = new Age(56, 61, 43);
     expect(galatic.getFutureYears()).toEqual({earthYears: 5, mercuryYears: 20.83, venusYears: 8.06, marsYears: 2.65, jupiterYears: 0.42});
   });
-
 
 });
