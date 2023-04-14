df2c945ecd683c5e9eca68fd5a61e41dcac3e571
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 10:00:36 -0700

    Updated first test to account for future birthday and past birthday

--- __tests__/galatic-age.test.js
+++ __tests__/galatic-age.test.js
@@ -2,11 +2,10 @@ import Age from './../src/js/galatic-age.js';
 
 describe('Age', () => {
 
-  test('should correctly create a Age object', () => {
-    const galatic = new Age(10);
-    expect(galatic.mercury).toEqual(2.4);
+  test('should correctly create a Age object with three ages', () => {
+    const galatic = new Age(10, 15, 5);
+    expect(galatic).toEqual({earth: 10, futureB: 15, passedB: 5});
   });
-
 
 
 
