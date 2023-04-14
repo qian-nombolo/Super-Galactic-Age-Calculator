502ef94fdac0b8cc42a12d637bfa8f4f1aad6cc7
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 13:11:24 -0700

    Pass third test

--- src/js/galatic-age.js
+++ src/js/galatic-age.js
@@ -25,6 +25,20 @@ export default class Age {
     };
   }  
   
-  
+  getFutureYears() {
+    let earthYears = this.futureB - this.earth;
+    let mercuryYears = Math.floor((earthYears / 0.24)*100) / 100;
+    let venusYears = Math.floor((earthYears / 0.62)* 100) / 100;
+    let marsYears = Math.floor((earthYears / 1.88)* 100) / 100;
+    let jupiterYears = Math.floor((earthYears / 11.86)* 100) / 100;
+
+    return {
+      earthYears: earthYears,
+      mercuryYears: mercuryYears,
+      venusYears: venusYears,
+      marsYears: marsYears,
+      jupiterYears: jupiterYears
+    };
+  }
 
 }
