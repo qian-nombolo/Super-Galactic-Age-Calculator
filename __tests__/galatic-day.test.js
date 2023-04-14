84d1b183ba5970a59df6962510d29ce92851d4ab
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 16:29:29 -0700

    Add fifth test

--- __tests__/galatic-day.test.js
+++ __tests__/galatic-day.test.js
@@ -3,8 +3,13 @@ import Day from './../src/js/galatic-day.js';
 describe('Day', () => {
 
   test('should correctly create a Day object with year, month and date', () => {
-    const birthday = new Day(1993, 10, 01);
+    const birthday = new Day(1993, 10, 1);
     expect(birthday.month).toEqual(10);
+  });
+
+  test('should correctly calculate how many days to pass until a future birthday', () => {
+    const birthday = new Day(1993, 10, 1);
+    expect(birthday.getDays().earthDays).toEqual(365);
   });
 
 });
