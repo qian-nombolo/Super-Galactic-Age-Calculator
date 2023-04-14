20dcaadbfa2f4678cf358ad03e8c7ec75226da88
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-14 10:12:10 -0700

    Updated expectation in test 1

--- __tests__/galatic-age.test.js
+++ __tests__/galatic-age.test.js
@@ -4,7 +4,7 @@ describe('Age', () => {
 
   test('should correctly create a Age object with three ages', () => {
     const galatic = new Age(10, 15, 5);
-    expect(galatic).toEqual({earth: 10, futureB: 15, passedB: 5});
+    expect(galatic.futureB).toEqual(15);
   });
 
 
